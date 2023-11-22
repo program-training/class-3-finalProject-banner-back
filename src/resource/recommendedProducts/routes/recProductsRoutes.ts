29ceea793acab20e72c08959dbcc9bbec0f33acc
ari <ar10001010@gmail.com>
2023-11-22 14:23:45 +0200

    fixed main and dabug

--- src/resource/recommendedProducts/routes/recProductsRoutes.ts
+++ src/resource/recommendedProducts/routes/recProductsRoutes.ts
@@ -1,18 +1,18 @@
 import express from "express";
-import {
-  handleGetRecProducts,
-  handleGetRecProductById,
-  handleGetRecProductByproductId,
-  handlePostRecProducts,
-  handleDeleteRecProducts,
-} from "../controllers/recProductsControllers";
+// import {
+//   handleGetRecProducts,
+//   handleGetRecProductById,
+//   handleGetRecProductByproductId,
+//   handlePostRecProducts,
+//   handleDeleteRecProducts,
+// } from "../controllers/recProductsControllers";
 
 const router = express.Router();
 
-router.get("/recProducts", handleGetRecProducts);
-router.get("/recProducts:recProductId", handleGetRecProductById);
-router.get("/recProducts:productId", handleGetRecProductByproductId);
-router.post("/recProducts", handlePostRecProducts);
-router.delete("/recProducts:recProductsId", handleDeleteRecProducts);
+// router.get("/recProducts", handleGetRecProducts);
+// router.get("/recProducts:recProductId", handleGetRecProductById);
+// router.get("/recProducts:productId", handleGetRecProductByproductId);
+// router.post("/recProducts", handlePostRecProducts);
+// router.delete("/recProducts:recProductsId", handleDeleteRecProducts);
 
 export default router;
