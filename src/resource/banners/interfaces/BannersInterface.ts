f2bab77f0864bd1fbcc492fd7ebe8e3c59d71f8d
ari <ar10001010@gmail.com>
2023-11-26 16:59:02 +0200

    add eslint

--- src/resource/banners/interfaces/BannersInterface.ts
+++ src/resource/banners/interfaces/BannersInterface.ts
@@ -1,17 +1,16 @@
 import { Types } from "mongoose";
 
-
-
 export default interface BannerInterface {
-  _id: Types.ObjectId;
-  category: string
+  _id: string;
+  category: string;
   image: {
-      url: string;
-      alt: string;
-  }
-  title: string
+    large: string;
+    medium: string;
+    small: string;
+    alt: string;
+  };
+  title: string;
   text: string;
   createAt: Date;
-  author: Types.ObjectId;
-  
+  author: string;
 }
