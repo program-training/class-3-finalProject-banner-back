307e46010eee3025b23cdf7bb6b869aa6ca0ace6
Ariel Zamir <arielzamir100@gmail.com>
2023-12-10 21:18:04 +0200

    Changed the logsSchema due to errors

--- src/mongoDB/Schemas/LogsSchema.ts
+++ src/mongoDB/Schemas/LogsSchema.ts
@@ -1,13 +1,11 @@
 import mongoose, { Schema, Model, Document } from "mongoose";
 import { LogsInterface } from "../../resource/logs/interfaces/logInterface";
 
-interface LogDocument extends LogsInterface, Document {}
-
-const logSchema = new Schema<LogDocument>({
+const logSchema = new Schema<LogsInterface>({
   shortDate: { type: String, required: true },
   date: { type: String, required: true },
 });
 
-const logModel: Model<LogDocument> = mongoose.model("log", logSchema);
+const logModel: Model<LogsInterface> = mongoose.model("log", logSchema);
 
 export default logModel;
