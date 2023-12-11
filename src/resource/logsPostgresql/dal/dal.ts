6d9cf3ab1bbce4eed2697fa35ee5b71899984845
Ariel Zamir <arielzamir100@gmail.com>
2023-12-11 19:28:19 +0200

    Removed the connectToSql due to another file that does the same, plus i adjusted the dal in logsPostgresql to support it

--- src/resource/logsPostgresql/dal/dal.ts
+++ src/resource/logsPostgresql/dal/dal.ts
@@ -1,12 +1,13 @@
-import { ConnectToSQL } from "../../../postgreSQL/postgresqlConnection";
+import { pool } from "../../../postgresql/connectToDBPg";
 
 export const getAllHoursDal = async (dateId: String) => {
   try {
     const arrayOfHours = Array(24).fill(0);
-    const foundDate = await ConnectToSQL().query(
+    const foundDate = await pool.query(
       `SELECT * FROM logs WHERE shortDate = $1`,
       [dateId]
     );
+
     if (foundDate.rows.length > 0) {
       for (const element of foundDate.rows) {
         const dateString = element.date;
